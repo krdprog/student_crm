50b0f0c1bc88ec47f5f8342468692efe4d06435a
Alexey Tsaplin-Kupaysinov <info@krdprog.ru>
2020-12-19 19:22:12 +0300

    Add seeds with sample data for Students

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def male_first_names
+  %w[Андрей Сергей Михаил Василий Денис Николай Виктор Дмитрий Владимир Олег].sample
+end
+
+def male_middle_names
+  %w[Андреевич Сергеевич Михаилович Васильевич Денисович Николаевич Олегович].sample
+end
+
+def male_last_names
+  %w[Уваров Глызов Васильев Кириллов Зайцев Волков Скворцов Онегин Малышев Осипов].sample
+end
+
+11.times do
+  random_signal = (6..16).map { ('a'..'z').to_a[rand(26)] }.join.upcase
+
+  Student.find_or_create_by(first_name: male_first_names,
+                 middle_name: male_middle_names,
+                 last_name: male_last_names,
+                 gender: 0,
+                 signal: random_signal)
+end
+
+def female_first_names
+  %w[Анфиса Лариса Ольга Мария Лидия Валерия Анна Марина Екатерина].sample
+end
+
+def female_middle_names
+  %w[Сергеевна Олеговна Алексеевна Александровна Вячеславовна Ульяновна].sample
+end
+
+def female_last_names
+  %w[Репина Хромова Боярская Аксенова Багрова Костикова Пирогова Мастерова].sample
+end
+
+12.times do
+  Student.find_or_create_by(first_name: female_first_names,
+                 middle_name: female_middle_names,
+                 last_name: female_last_names,
+                 gender: 1)
+end
+
+puts 'Seeds OK!'
