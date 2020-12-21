ee17b8819d3b2edf19e50618746614b663ce99c0
Alexey Tsaplin-Kupaysinov <info@krdprog.ru>
2020-12-21 15:17:16 +0300

    Fix quotes in test

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -28,42 +28,42 @@ class StudentTest < ActiveSupport::TestCase
   end
 
   test 'invalid if first_name is too long' do
-    @student.update(first_name: "A" * 41)
+    @student.update(first_name: 'A' * 41)
 
     assert @student.invalid?
   end
 
   test 'invalid if middle_name is too long' do
-    @student.update(middle_name: "A" * 61)
+    @student.update(middle_name: 'A' * 61)
 
     assert @student.invalid?
   end
 
   test 'invalid if last_name is too long' do
-    @student.update(last_name: "A" * 41)
+    @student.update(last_name: 'A' * 41)
 
     assert @student.invalid?
   end
 
   test 'invalid if signal is too long' do
-    @student.update(signal: "A" * 17)
+    @student.update(signal: 'A' * 17)
 
     assert @student.invalid?
   end
 
   test 'invalid if signal is too short' do
-    @student.update(signal: "A" * 5)
+    @student.update(signal: 'A' * 5)
 
     assert @student.invalid?
   end
 
   test 'invalid if signal is not uniq' do
     Student.create(
-      first_name: "String",
-      middle_name: "String",
-      last_name: "String",
+      first_name: 'String',
+      middle_name: 'String',
+      last_name: 'String',
       gender: 0,
-      signal: "QWERTY"
+      signal: 'QWERTY'
     )
 
     @student.update(signal: 'QWERTY')
